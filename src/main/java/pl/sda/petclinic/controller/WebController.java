069d1bc25de782711a290b8c7da17eebd89f374c
palec911 <plh911@live.com>
2023-07-10 14:57:10 +0200

    MVC List Pets

--- src/main/java/pl/sda/petclinic/controller/WebController.java
+++ src/main/java/pl/sda/petclinic/controller/WebController.java
@@ -3,14 +3,29 @@ package pl.sda.petclinic.controller;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.sda.petclinic.services.PetService;
 
 @Controller
 public class WebController {
+
+    PetService petService;
+
+    public WebController(PetService petService) {
+        this.petService = petService;
+    }
+
     @GetMapping("/hello")
     public String homepage(Model model) {
         model.addAttribute("theDate", new java.util.Date());
 
         return "helloworld";
 
+    }
+
+    @GetMapping("/pets")
+    public String getPets(Model model) {
+        model.addAttribute("pets", petService.getAll());
+
+        return "list-pets";
     }
 }
