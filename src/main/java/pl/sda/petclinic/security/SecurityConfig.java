f546eec8c61c281becdccf29d4bb41f261fbd3ac
palec911 <plh911@live.com>
2023-07-10 22:23:58 +0200

    Custom JDBC

--- src/main/java/pl/sda/petclinic/security/SecurityConfig.java
+++ src/main/java/pl/sda/petclinic/security/SecurityConfig.java
@@ -8,33 +8,51 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager() {
+//        UserDetails ppalczew = User.builder()
+//                .username("ppalczew")
+//                .password("{noop}test123")
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails ahyz = User.builder()
+//                .username("ahyz")
+//                .password("{noop}test123")
+//                .roles("USER", "VET")
+//                .build();
+//
+//        UserDetails sda = User.builder()
+//                .username("sda")
+//                .password("{noop}test123")
+//                .roles("USER", "VET", "ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(sda,ppalczew,ahyz);
+//    }
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails ppalczew = User.builder()
-                .username("ppalczew")
-                .password("{noop}test123")
-                .roles("USER")
-                .build();
-
-        UserDetails ahyz = User.builder()
-                .username("ahyz")
-                .password("{noop}test123")
-                .roles("USER", "VET")
-                .build();
-
-        UserDetails sda = User.builder()
-                .username("sda")
-                .password("{noop}test123")
-                .roles("USER", "VET", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(sda,ppalczew,ahyz);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?"
+        );
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?"
+        );
+        return jdbcUserDetailsManager;
     }
+
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests( config ->
